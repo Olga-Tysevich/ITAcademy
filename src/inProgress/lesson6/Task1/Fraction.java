03a856707720d7dcd9c479887a68ab69b8952a69
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-08-31 12:07:09 +0300

    Updated task #1 for lesson6

--- src/inProgress/lesson6/Task1/Fraction.java
+++ src/inProgress/lesson6/Task1/Fraction.java
@@ -13,51 +13,39 @@ public class Fraction {
         double sum = (double) numerator / denominator + summand;
         int numerator = (int) (Math.round(sum * this.denominator * 1000));
         int denominator = this.denominator * 1000;
-        int[] arrayReducedFraction = reduceFraction(numerator, denominator);
-        numerator = arrayReducedFraction[0];
-        denominator = arrayReducedFraction[1];
 
-        return new Fraction(numerator, denominator);
+        return reduceFraction(numerator, denominator);
     }
 
     public Fraction addFractions(int numeratorOfSecondSummand, int denominatorOfSecondSummand) {
         int numerator = (numeratorOfSecondSummand * this.denominator + this.numerator * denominatorOfSecondSummand);
         int denominator = (denominatorOfSecondSummand * this.denominator);
-        int[] arrayReducedFraction = reduceFraction(numerator, denominator);
-        numerator = arrayReducedFraction[0];
-        denominator = arrayReducedFraction[1];
 
-        return new Fraction(numerator, denominator);
+        return reduceFraction(numerator, denominator);
     }
 
     public Fraction multiplyFractions(double multiplier) {
         int numerator = this.numerator * (int) Math.round(multiplier * 100);
         int denominator = this.denominator * 100;
-        int[] arrayReducedFraction = reduceFraction(numerator, denominator);
-        numerator = arrayReducedFraction[0];
-        denominator = arrayReducedFraction[1];
 
-        return new Fraction(numerator, denominator);
+        return reduceFraction(numerator, denominator);
     }
 
     public Fraction divideFractions(double divisor) {
         int numerator = this.numerator * (int) Math.round(1 / divisor * 100);
         int denominator = this.denominator * 100;
-        int[] arrayReducedFraction = reduceFraction(numerator, denominator);
-        numerator = arrayReducedFraction[0];
-        denominator = arrayReducedFraction[1];
 
-        return new Fraction(numerator, denominator);
+        return reduceFraction(numerator, denominator);
     }
 
-    private int[] reduceFraction(int numerator, int denominator) {
+    private Fraction reduceFraction(int numerator, int denominator) {
         for (int i = 1000; i > 0; i--) {
             if (numerator % i == 0 && denominator % i == 0) {
                 numerator /= i;
                 denominator /= i;
             }
         }
-        return new int[]{numerator, denominator};
+        return new Fraction(numerator,denominator);
     }
 
     public void printFraction(String message) {
