0547ab0980f6125126c3393cdd5713c970e7fc7e
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-09-16 16:55:44 +0300

    Added lesson #10-11 task #1

---
+++ src/inProgress/lesson10_11/pair/Pair.java
@@ -0,0 +1,38 @@
+package inProgress.lesson10_11.pair;
+
+public class Pair<T> {
+    private T first;
+    private T last;
+
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public void swap() {
+        T temp = first;
+        first = last;
+        last = temp;
+    }
+
+    public void replaceFirst(T newFirst) {
+        first = newFirst;
+    }
+
+    public void replaceLast(T newLast) {
+        last = newLast;
+    }
+
+    public T first() {
+        return first;
+    }
+
+    public T last() {
+        return last;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair {first = " + first + ", last = " + last + '}';
+    }
+}
---
+++ src/inProgress/lesson10_11/pair/PairDemo.java
@@ -0,0 +1,31 @@
+package inProgress.lesson10_11.pair;
+
+public class PairDemo {
+    public static void main(String[] args) {
+        Pair<String> stringPair = new Pair<>("first", "last");
+
+        System.out.println(stringPair.first());
+        System.out.println(stringPair.last());
+
+        stringPair.swap();
+        System.out.println("First after swap: " + stringPair.first() + "; last after swap: " + stringPair.last());
+
+        stringPair.replaceFirst("newFirst");
+        stringPair.replaceLast("newLast");
+
+        System.out.println(stringPair);
+
+        Pair<Integer> integerPair = new Pair<>(1, 2);
+
+        System.out.println(integerPair.first());
+        System.out.println(integerPair.last());
+
+        integerPair.swap();
+        System.out.println("First after swap: " + integerPair.first() + "; last after swap: " + integerPair.last());
+
+        integerPair.replaceFirst(3);
+        integerPair.replaceLast(4);
+
+        System.out.println(integerPair);
+    }
+}
