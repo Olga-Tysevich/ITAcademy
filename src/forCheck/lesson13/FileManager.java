69a0f0b690cdb4406ea21e011ab99891dae53233
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-09-29 16:02:49 +0300

    Lesson13 package moved to package forCheck

--- src/forCheck/lesson13/FileManager.java
+++ src/forCheck/lesson13/FileManager.java
@@ -1,4 +1,4 @@
-package inProgress.lesson13;
+package forCheck.lesson13;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -8,11 +8,11 @@ import java.util.Random;
 public class FileManager {
     Random random;
 
-    public void writeNumbersToFile(String outFilePath) {
+    public void writePosNumbersToFile(String outFilePath, int amountOfNumbers, int upperValueLimit) {
         try (FileWriter fileWriter = new FileWriter(outFilePath)) {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < amountOfNumbers; i++) {
                 random = new Random();
-                fileWriter.write(random.nextInt(100001) + "\n");
+                fileWriter.write((random.nextInt(upperValueLimit) + 1) + "\n");
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -33,31 +33,6 @@ public class FileManager {
         }
     }
 
-    public void writeBinaryFile(String outFilePath) {
-        try (OutputStream outputStream = new FileOutputStream(outFilePath)) {
-            random = new Random();
-            for (int i = 1; i < 31; i++) {
-                outputStream.write(random.nextInt(101));
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void readBinaryFile(String filePath) {
-        try (InputStream inputStream = new FileInputStream(filePath)) {
-            int current;
-            double sum = 0;
-            while ((current = inputStream.read()) != -1) {
-                System.out.print(current + " ");
-                sum += current;
-            }
-            System.out.println("\n" + (sum / 30));
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     private ArrayList<Integer> copyFileDataToArrayList(ArrayList<Integer> arrayList, String filePath) {
         String str1;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
@@ -69,4 +44,31 @@ public class FileManager {
         }
         return arrayList;
     }
+
+    public void writePosNumbersToBinaryFile(String outFilePath, int amountOfNumbers, int upperValueLimit) {
+        try (OutputStream outputStream = new FileOutputStream(outFilePath)) {
+            random = new Random();
+            for (int i = 0; i < amountOfNumbers; i++) {
+                outputStream.write(random.nextInt(upperValueLimit) + 1);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void printNumbersFromBinaryFile(String filePath) {
+        try (InputStream inputStream = new FileInputStream(filePath)) {
+            int current, numberCounter = 0;
+            double sum = 0;
+            while ((current = inputStream.read()) != -1) {
+                System.out.print(current + " ");
+                sum += current;
+                numberCounter++;
+            }
+            System.out.println("\nArithmetic mean: " + (sum / numberCounter));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
