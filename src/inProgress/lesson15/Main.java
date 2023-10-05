341432d9be2da7385a4b59dea8180195e3ead82c
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-10-05 11:48:34 +0300

    Added task #1 lesson15

---
+++ src/inProgress/lesson15/Main.java
@@ -0,0 +1,26 @@
+package inProgress.lesson15;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        //task #1
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            list.add(random.nextInt(100) + 1);
+        }
+        list.forEach(s -> System.out.print(s + " "));
+
+        System.out.println("\nMin: " + list.stream().min(Integer::compare).get());
+        System.out.println("Max: " + list.stream().max(Integer::compare).get());
+        System.out.println("Average: " + list.stream().mapToInt(Integer::intValue).average().getAsDouble());
+        System.out.println("Product of numbers: " + list.stream().mapToLong(Integer::longValue).reduce((x, y) -> x * y).getAsLong());
+        System.out.println("Sum of all numbers: " + list.stream().mapToInt(Integer::intValue).sum());
+        System.out.println("Sum of all digits: " + Arrays.stream(list.stream().map(String::valueOf).collect(Collectors.joining()).split(""))
+                .mapToInt(Integer::parseInt).sum());
+
+
+    }
+}
