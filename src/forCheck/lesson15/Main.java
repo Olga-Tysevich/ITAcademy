bfb44af12437d1f05ea31b0cf36bd2d2c03672e2
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-10-06 12:06:32 +0300

    Lesson 15 package has been moved to package forCheck

--- src/forCheck/lesson15/Main.java
+++ src/forCheck/lesson15/Main.java
@@ -1,4 +1,4 @@
-package inProgress.lesson15;
+package forCheck.lesson15;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -13,22 +13,16 @@ public class Main {
         }
         list.forEach(s -> System.out.print(s + " "));
 
-        System.out.println("\nMin: " + list.stream().min(Integer::compare).get());
-        System.out.println("Max: " + list.stream().max(Integer::compare).get());
-        System.out.println("Average: " + list.stream().mapToInt(Integer::intValue).average().getAsDouble());
-        System.out.println("Product of numbers: " + list.stream().mapToLong(Integer::longValue).reduce((x, y) -> x * y).getAsLong());
+        list.stream().min(Integer::compare).ifPresent(min -> System.out.println("\nMin: " + min));
+        list.stream().max(Integer::compare).ifPresent(max -> System.out.println("Max: " + max));
+        list.stream().mapToInt(Integer::intValue).average().ifPresent(average -> System.out.println("Average: " + average));
+        list.stream().mapToLong(Integer::longValue).reduce((x, y) -> x * y).ifPresent(product -> System.out.println("Product of numbers: " + product));
         System.out.println("Sum of all numbers: " + list.stream().mapToInt(Integer::intValue).sum());
         System.out.println("Sum of all digits: " + Arrays.stream(list.stream().map(String::valueOf).collect(Collectors.joining()).split(""))
                 .mapToInt(Integer::parseInt).sum());
 
         //task #2
         List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
-        myList.stream().filter(s -> !s.contains("3")).forEach(s -> System.out.print(s + " "));
-        System.out.println();
-        myList.stream().filter(s -> !s.contains("3"))
-                .sorted(Comparator.<String>comparingInt(s -> Integer.parseInt(s.split("")[1]))
-                        .thenComparing(Comparator.<String, String>comparing(s -> s.split("")[0]).reversed()))
-                .forEach(s -> System.out.print(s + " "));
         System.out.println();
         List<String> result = myList.stream().filter(s -> !s.contains("3"))
                 .sorted(Comparator.<String>comparingInt(s -> Integer.parseInt(s.split("")[1]))
