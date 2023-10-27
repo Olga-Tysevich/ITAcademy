cddc6069cde5439c3ca1c74960ed5cf39b0c188d
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-10-27 12:02:32 +0300

    Added homework #20

---
+++ src/forCheck/lesson20/MainController.java
@@ -0,0 +1,20 @@
+package forCheck.lesson20;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainController {
+    private double item1;
+    private double item2;
+    @FXML
+    TextField inputField;
+    @FXML
+    Button button1;
+
+//    public void startBtnClick(ActionEvent actionEvent) {
+//        System.out.println("button clicked");
+//        startBtn.setText("clicked");
+//    }
+}
