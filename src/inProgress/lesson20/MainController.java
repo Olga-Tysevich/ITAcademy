76eeff21260c63e623a504d863aa047c6b4cdc34
Olga Tysevich <Olga.Tisevich@gmail.com>
2023-10-27 13:42:24 +0300

    Added homework #20

--- src/inProgress/lesson20/MainController.java
+++ src/inProgress/lesson20/MainController.java
@@ -8,70 +8,43 @@ import javafx.scene.control.TextField;
 public class MainController {
     @FXML
     TextField inputField;
-    @FXML
-    Button button1;
-    @FXML
-    Button button2;
-    @FXML
-    Button button3;
-    @FXML
-    Button button4;
-    @FXML
-    Button button5;
-    @FXML
-    Button button6;
-    @FXML
-    Button button7;
-    @FXML
-    Button button8;
-    @FXML
-    Button button9;
-    @FXML
-    Button button0;
-    @FXML
-    Button buttonDot;
-    @FXML
-    Button buttonSum;
-    @FXML
-    Button buttonSub;
-    @FXML
-    Button buttonMult;
-    @FXML
-    Button buttonDiv;
-    @FXML
-    Button buttonEquals;
 
-    private double item1 = 0;
-    private double item2 = 0;
+    private double item = 0;
     private boolean start = true;
     private String operator = "";
-    private Calculator calculator = new Calculator();
+    private final Calculator calculator = new Calculator();
 
     @FXML
     private void getNum(ActionEvent event) {
-        if (start) {
-            inputField.setText("");
-            start = false;
-        }
         String value = ((Button) event.getSource()).getText();
-        inputField.setText(inputField.getText() + value);
+        if (start) {
+            inputField.setText(value);
+            start = false;
+        } else {
+            inputField.setText(inputField.getText() + value);
+        }
     }
 
     @FXML
     private void getOperator(ActionEvent event) {
         String value = ((Button) event.getSource()).getText();
+        if (".".equals(value) && !inputField.getText().contains(".")) {
+            inputField.setText(inputField.getText() + value);
+        } else if (".".equals(value) && inputField.getText().contains(".")) {
+            return;
+        }
         if (!"=".equals(value)) {
             if (!operator.isEmpty()) {
                 return;
             }
             operator = value;
-            item1 = Double.parseDouble(inputField.getText());
+            item = Double.parseDouble(inputField.getText());
             inputField.setText("");
         } else {
             if (operator.isEmpty()) {
                 return;
             }
-            inputField.setText(String.valueOf(calculator.calculate(item1, Double.parseDouble(inputField.getText()), operator)));
+            inputField.setText(String.valueOf(calculator.calculate(item, Double.parseDouble(inputField.getText()), operator)));
             operator = "";
             start = true;
         }
